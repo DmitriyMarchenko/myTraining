55800f7ea323a8ff11a77d72deed9e605b1c6ee8
Dmitiry <dmboxbat@gmail.com>
2017-12-18 09:49:22 +0200

    Change comment from class homeWork05

--- src/lesson03/homeWork05.java
+++ src/lesson03/homeWork05.java
@@ -4,14 +4,29 @@ import java.util.Scanner;
 
 public class homeWork05 {
 	private static Scanner scan05;
-	
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		scan05 = new Scanner(System.in);
-		//Задаем размер квадратной матрицы из конслоли
+		// Set the size of a square matrix from the console
 		System.out.print("Введите размер матрицы: ");
 		int size = scan05.nextInt();
 		System.out.print("Вы ввели: " + size);
 		scan05.close();
-	//	int [][] matrix = new int[size][size];
+		// part 1 - create matrix size * size
+		int[][] matrix = new int[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				matrix[i][j] = size * i + j;
+			}
+		}
+		// Part 2 - displays the initial matrix
+		System.out.println();
+		System.out.println("Начальная матрица");
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				System.out.printf("%4d", matrix[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
