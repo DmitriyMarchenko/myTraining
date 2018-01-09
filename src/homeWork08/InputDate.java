7b3165f05f7df4f236ff8a2baf48052aa2c71953
Dmitiry <dmboxbat@gmail.com>
2018-01-09 21:52:16 +0200

    Corrected class InputDate

--- src/homeWork08/InputDate.java
+++ src/homeWork08/InputDate.java
@@ -5,13 +5,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class InputDate {
-	public static void input() throws IOException {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-	String str;
+	public static  void annotation() {
 	System.out.println("Bвeдитe строки текста.");
-	System.out.println("Bвeдитe 'exit' для завершения."); 
-	do { str = br. readLine() + System.lineSeparator(); 
-	SaveData.save(str) ;
-	} while(!str.equals("exit")); 
+	System.out.println("Bвeдитe 'exit' для завершения.");
+	}
+
+	public static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		str = br.readLine();
+		boolean exit = str.equals("exit");
+		if(exit !=true) {
+			str = str + System.lineSeparator();
+			SaveData.save(str);	
+			input();
+		}
+	else {
+		return;
+		}
 	}
 }
+
+
+//do { str = br. readLine() + System.lineSeparator();
+		// SaveData.save(str) ;
+		// } while(!str.equals("exit"));
--- src/homeWork08/TextEditor.java
+++ src/homeWork08/TextEditor.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class TextEditor {
 	public static void main(String args[]) throws IOException {
+		InputDate.annotation();
 		//InputDate.input();
 		ReadData.read();
 	}
